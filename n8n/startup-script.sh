27dfb29304cbde5032ca598a389397bb48657d80
Christian Münch <christian@muench-worms.de>
2023-07-10 12:04:41 +0200

    Try to make the statup process of n8n more stable

--- n8n/startup-script.sh
+++ n8n/startup-script.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -e
 
-# Wait for MySQL to be ready
-until mysqladmin ping -h"$DB_MYSQLDB_HOST" --silent; do
-    sleep 1
-done
+echo "Waiting for MySQL to be ready ..."
+# Use wait-for-it to wait for MySQL to be ready
+/wait-for-it.sh "$DB_MYSQLDB_HOST:3306" --timeout=30 --strict -- echo "MySQL is up"
 
+echo "Creating the database n8n ..."
 # Create the database
 mysql -u"root" -p"root" -h"$DB_MYSQLDB_HOST" -e "CREATE DATABASE IF NOT EXISTS \`$DB_MYSQLDB_DATABASE\`; GRANT ALL PRIVILEGES ON n8n.* TO 'db'@'%'; FLUSH PRIVILEGES;"
 
+echo "Starting n8n..."
 # Start n8n
 exec n8n
